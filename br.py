68fb402d7f7e087dd8dc2cbd2095cc2c330b0be4
Srivatsava Gade <66792412+srivatsavagade@users.noreply.github.com>
2023-07-19 22:15:43 +0530

    Update br.py

--- br.py
+++ br.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from rembg import remove
 from PIL import Image
+import io
 
 def main():
     st.title("Background Removal App")
@@ -16,11 +17,17 @@ def main():
                 result_image = remove(image)
                 st.image(result_image, caption='Background Removed', use_column_width=True)
 
+                # Convert the PIL Image to bytes
+                result_image_bytes = io.BytesIO()
+                result_image.save(result_image_bytes, format='PNG')
+                result_image_bytes = result_image_bytes.getvalue()
+
                 # Create a download button for the background-removed image
                 st.download_button(
-                label="Download Background Removed Image",
-                data=result_image,
-                file_name="background_removed.png")
+                    label="Download Background Removed Image",
+                    data=result_image_bytes,
+                    file_name="background_removed.png"
+                )
 
 if __name__ == "__main__":
     main()
